f84adf08b4d896138da86efb7e2e9f7797f5544e
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2011-10-05 23:22:33 +0000

    Borramos db4o que no tiene soporte desde spring (al menos no uno maduro).

--- mg-bdoo-tp-items/src/installer/ItemTrackerInstaller.java
+++ mg-bdoo-tp-items/src/installer/ItemTrackerInstaller.java
@@ -5,27 +5,24 @@ import user.domain.User;
 
 import itemTracker.domain.ItemTracker;
 
-import com.db4o.Db4oEmbedded;
-import com.db4o.ObjectContainer;
-
 public class ItemTrackerInstaller {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ObjectContainer db=Db4oEmbedded.openFile(Db4oEmbedded
-				.newConfiguration(), "SeguimientoDeItemsBaseDeDatos");
-		try {
-			ItemTracker itemTracker = ItemTracker.getInstance();
-			User aUser = new User("rodrigo","rodrigo");
-			itemTracker.addUser(aUser);
-			db.store(itemTracker);
-			System.out.println("Stored: "+itemTracker);
-			System.out.println("Stored: "+aUser);
-		}
-		finally {
-			db.close();
-		}
+//		ObjectContainer db=Db4oEmbedded.openFile(Db4oEmbedded
+//				.newConfiguration(), "SeguimientoDeItemsBaseDeDatos");
+//		try {
+//			ItemTracker itemTracker = ItemTracker.getInstance();
+//			User aUser = new User("rodrigo","rodrigo");
+//			itemTracker.addUser(aUser);
+//			db.store(itemTracker);
+//			System.out.println("Stored: "+itemTracker);
+//			System.out.println("Stored: "+aUser);
+//		}
+//		finally {
+//			db.close();
+//		}
 	}
 }
