fa951a02b760bbb766fcc2b10e8abaa35682a2a5
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-03-17 07:11:16 +0000

    Se avanza sobre ka configuracion de spring: se divide el applicationContext en varios archivos para que sea mas manejable. Tambien se cambian de lugares mapeos.

--- mg-bdoo-tp-items/src/base/repository/AbstractRepositoryFinder.java
+++ mg-bdoo-tp-items/src/base/repository/AbstractRepositoryFinder.java
@@ -1,13 +1,25 @@
 package base.repository;
 
+import itemTracker.repository.AbstractItemTrackerRepository;
 import user.repository.AbstractUserRepository;
 
 public abstract class AbstractRepositoryFinder {
 
+	AbstractItemTrackerRepository itemTrackerRepository;
 	AbstractUserRepository userRepository;
+
+	public AbstractItemTrackerRepository getItemTrackerRespository(){
+		return this.itemTrackerRepository;
+	}
+	public void setUserRespository(AbstractItemTrackerRepository itemTrackerRepository){
+		this.itemTrackerRepository = itemTrackerRepository;
+	}
 	
 	public AbstractUserRepository getUserRespository(){
 		return this.userRepository;
+	}
+	public void setUserRespository(AbstractUserRepository userRepository){
+		this.userRepository = userRepository;
 	}
 	
 }
