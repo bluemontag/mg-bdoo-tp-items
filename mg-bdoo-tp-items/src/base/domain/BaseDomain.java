00d78c7972b3dd72488e07a2e68f120b7fc41e82
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-04-06 22:29:15 +0000

    Vamos a ver si se necesita una clase base de dominio

--- mg-bdoo-tp-items/src/base/domain/BaseDomain.java
+++ mg-bdoo-tp-items/src/base/domain/BaseDomain.java
@@ -2,16 +2,26 @@ package base.domain;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
+ * TODO: creo que esto no va...
  */
-public class BaseDomain {
+public abstract class BaseDomain {
 
-	private Long id;
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getId() {
-		return id;
-	}
+//	private Long id;
+//	private Integer version;
+//	
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setVersion(Integer version) {
+//		this.version = version;
+//	}
+//
+//	public Integer getVersion() {
+//		return version;
+//	}
 }
