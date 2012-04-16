3749a35c2e4e0fcbb2d809a61438edf6a4b801c4
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-04-16 02:07:27 +0000

    Exception para cuando se intenta modificar un objeto que ya fue modificado "a nivel DTO".

---
+++ mg-bdoo-tp-items/src/base/exception/DTOConcurrencyException.java
@@ -0,0 +1,11 @@
+package base.exception;
+
+public class DTOConcurrencyException extends BaseException {
+
+	public DTOConcurrencyException(String msj) {
+		this.setMsj(msj);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
