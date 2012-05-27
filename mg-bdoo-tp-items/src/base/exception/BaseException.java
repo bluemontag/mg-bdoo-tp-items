193e327a1a0915464d45e6fe9c5c3b5e937bdb8d
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-27 05:21:58 +0000

    constructor

--- mg-bdoo-tp-items/src/base/exception/BaseException.java
+++ mg-bdoo-tp-items/src/base/exception/BaseException.java
@@ -8,6 +8,9 @@ public class BaseException extends Exception{
 	private static final long serialVersionUID = 1L;
 	protected String msj="";
 	
+	public BaseException(String msj) {
+		this.msj = msj;
+	}
 	public void setMsj(String msj){
 		this.msj = msj;
 	}
