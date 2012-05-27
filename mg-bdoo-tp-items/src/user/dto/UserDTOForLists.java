136865d740ebaa8ae27bd77266bb57ca7ccb6cee
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-27 23:42:24 +0000

    desarrollo del dto para listas

--- mg-bdoo-tp-items/src/user/dto/UserDTOForLists.java
+++ mg-bdoo-tp-items/src/user/dto/UserDTOForLists.java
@@ -5,7 +5,47 @@ import base.dto.AbstractDTOForLists;
 
 public class UserDTOForLists extends AbstractDTOForLists{
 
-	public UserDTOForLists(User aBaseDomainObject) {
+	private String userName;
+	private String password;
+	private boolean removed;
+
+	
+	public UserDTOForLists(User anUser) {
+		super(anUser);
+		this.userName = anUser.getUserName();
+		this.password = anUser.getPassword();
+		this.removed = anUser.isRemoved();
+	}
+	
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
 	}
 
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String toString(){
+		return "UserName: - "+this.getUserName()+" - "+ (this.removed? "REMOVED": "ALIVE");
+	}
 }
