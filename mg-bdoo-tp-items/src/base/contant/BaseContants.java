ec8d2ca5232a0a5df83bc2b7d3a5691bb444d6eb
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-27 05:20:52 +0000

    constantes de configuracion

---
+++ mg-bdoo-tp-items/src/base/contant/BaseContants.java
@@ -0,0 +1,9 @@
+package base.contant;
+
+public class BaseContants {
+
+	public final static String CONTEXT_FILE = "applicationContext.xml";
+	public final static String DEFAULT_ADMIN_USER_NAME = "rodrigo";
+	public final static String DEFAULT_ADMIN_PASSWORD = "rodrigo";
+	
+}
