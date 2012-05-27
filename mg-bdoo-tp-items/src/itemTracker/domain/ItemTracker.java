25f632b463a0da3073ed70040310f75244b7b8bd
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-27 05:25:43 +0000

    se agregan los proyectos y el administrador del sistema.

--- mg-bdoo-tp-items/src/itemTracker/domain/ItemTracker.java
+++ mg-bdoo-tp-items/src/itemTracker/domain/ItemTracker.java
@@ -3,6 +3,9 @@ package itemTracker.domain;
 import java.util.Collection;
 import java.util.HashSet;
 
+import proyect.domain.Proyect;
+import proyect.exception.UnknownProyectException;
+
 import base.domain.BaseDomain;
 
 import user.domain.User;
@@ -17,29 +20,44 @@ public class ItemTracker extends BaseDomain{
 	public final static String DESCRIPTION = "Trabajo practico - Mg. Ing. de Softare - BDOO";
 		
 	private Collection<User> users = new HashSet<User>();
+	private User adminUser = null;
+	private Collection<Proyect> proyects = new HashSet<Proyect>();
 
 	public ItemTracker(){
 		this.users = new HashSet<User>();
+		this.setProyects(new HashSet<Proyect>());
 	}
 	
-	/**
-	 * @return the users
-	 */
 	public void addUser(User aUser) {
 		this.users.add(aUser);
 	}
-	/**
-	 * @param users the users to set
-	 */
+	
 	public void setUsers(Collection<User> users) {
 		this.users = users;
 	}
 
-	/**
-	 * @returns users
-	 */
 	public Collection<User> getUsers() {
 		return this.users;
+	}
+
+	public User getAdminUser() {
+		return adminUser;
+	}
+
+	public void setAdminUser(User theAdminUser) {
+		this.adminUser = theAdminUser;
+	}
+	
+	public void setProyects(Collection<Proyect> proyects) {
+		this.proyects = proyects;
+	}
+
+	public Collection<Proyect> getProyects() {
+		return proyects;
+	}
+	
+	public void addProyect(Proyect aProyect){
+		this.proyects.add(aProyect);
 	}
 
 	public void logicalRemoveUser(User anUser) {
@@ -60,6 +78,27 @@ public class ItemTracker extends BaseDomain{
 			this.users.remove(anUserToRemove);
 		}else{
 			throw new UnknownUserException();
+		}
+	}
+	
+	public void logicalRemoveProyect(Proyect aProyect) {
+		aProyect.setRemoved(true);
+	}
+	
+	//usado solo por los tests para dejar la base como estaba
+	@Deprecated
+	public void removeProyect(Proyect aProyect) throws UnknownProyectException {
+		Proyect aProyectToRemove = null;
+		for(Proyect aProyectIndex: this.proyects){
+			if(aProyectIndex.equals(aProyect)){
+				aProyectToRemove = aProyectIndex;
+				break;
+			}
+		}
+		if(aProyectToRemove != null){
+			this.proyects.remove(aProyectToRemove);
+		}else{
+			throw new UnknownProyectException();
 		}
 	}
 }
