f86070929ae2ec36ad9c483e10afb3b21721e192
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-27 23:39:38 +0000

    test de proyecto

--- mg-bdoo-tp-items/src/base/test/BaseSuiteTest.java
+++ mg-bdoo-tp-items/src/base/test/BaseSuiteTest.java
@@ -1,13 +1,14 @@
 package base.test;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import base.contant.BaseContants;
-
-import user.test.UserUpdateServiceTest;
-
 import junit.framework.Test;
 import junit.framework.TestSuite;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import proyect.test.ProyectCreateServiceTest;
+import proyect.test.ProyectUpdateServiceTest;
+import user.test.UserUpdateServiceTest;
+import base.contant.BaseContants;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
@@ -16,11 +17,11 @@ public abstract class BaseSuiteTest {
 
 	@SuppressWarnings("rawtypes")
 	public static Test suite() {
-		
+
 		loadApplicationContext();
 
 		Class[] testClasses = getTestsToPerform();
-		TestSuite suite= new TestSuite(testClasses);
+		TestSuite suite = new TestSuite(testClasses);
 
 		return suite;
 	}
@@ -31,9 +32,10 @@ public abstract class BaseSuiteTest {
 	}
 
 	@SuppressWarnings("rawtypes")
-	protected static Class[] getTestsToPerform(){
-		Class[] testClasses = {UserUpdateServiceTest.class};
+	protected static Class[] getTestsToPerform() {
+		Class[] testClasses = { UserUpdateServiceTest.class, ProyectCreateServiceTest.class,
+				ProyectUpdateServiceTest.class };
 		return testClasses;
 	}
-	
+
 }
