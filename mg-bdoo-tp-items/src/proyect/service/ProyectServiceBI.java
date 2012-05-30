3ac92490a96006e3941541650a266d6b7e73e80a
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-30 04:54:53 +0000

    se agrega excepcion a un servicio

--- mg-bdoo-tp-items/src/proyect/service/ProyectServiceBI.java
+++ mg-bdoo-tp-items/src/proyect/service/ProyectServiceBI.java
@@ -8,6 +8,7 @@ import proyect.exception.UnknownProyectException;
 import user.dto.UserDTO;
 import user.dto.UserDTOForLists;
 import user.exception.UnknownUserException;
+import base.exception.DTOConcurrencyException;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
@@ -15,20 +16,22 @@ import user.exception.UnknownUserException;
 public interface ProyectServiceBI {
 
 	// Creats
-	
-	ProyectDTO createProyect(String aProyectName, UserDTO aProyectLeaderUserDTO) throws ProyectAlreadyExistsException, UnknownUserException;
-	
+
+	ProyectDTO createProyect(String aProyectName, UserDTO aProyectLeaderUserDTO) throws ProyectAlreadyExistsException,
+			UnknownUserException;
+
 	// Lists
-	
+
 	// Retrives
 
 	ProyectDTO getProyect(ProyectDTO anCreatedProyectDTO) throws UnknownProyectException;
-	
+
 	// Updates
-	
-	void addUsersToProyect(ProyectDTO aProyectDTO, Collection<UserDTOForLists> users) throws UnknownProyectException, UnknownUserException;
-	
+
+	void addUsersToProyect(ProyectDTO aProyectDTO, Collection<UserDTOForLists> users) throws UnknownProyectException,
+			UnknownUserException;
+
 	// Removes
-	
-	void removeProyect(ProyectDTO anProyectDTOToRemove) throws UnknownProyectException;
+
+	void removeProyect(ProyectDTO anProyectDTOToRemove) throws UnknownProyectException, DTOConcurrencyException;
 }
--- mg-bdoo-tp-items/src/proyect/service/ProyectServiceImpl.java
+++ mg-bdoo-tp-items/src/proyect/service/ProyectServiceImpl.java
@@ -3,6 +3,8 @@
  */
 package proyect.service;
 
+import itemTracker.domain.ItemTracker;
+
 import java.util.Collection;
 
 import proyect.domain.Proyect;
@@ -10,49 +12,48 @@ import proyect.dto.ProyectDTO;
 import proyect.dto.ProyectDTOFactory;
 import proyect.exception.ProyectAlreadyExistsException;
 import proyect.exception.UnknownProyectException;
-import itemTracker.domain.ItemTracker;
 import user.domain.User;
 import user.dto.UserDTO;
-import user.dto.UserDTOFactory;
 import user.dto.UserDTOForLists;
 import user.exception.UnknownUserException;
-import user.exception.UserAlreadyExistsException;
+import base.exception.DTOConcurrencyException;
 import base.service.AbstractServiceImpl;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
  */
-public class ProyectServiceImpl extends AbstractServiceImpl implements ProyectServiceBI{
+public class ProyectServiceImpl extends AbstractServiceImpl implements ProyectServiceBI {
 
 	@Override
-	public ProyectDTO createProyect(String aProyectName, UserDTO aProyectLeaderUserDTO) throws ProyectAlreadyExistsException, UnknownUserException{
-		
+	public ProyectDTO createProyect(String aProyectName, UserDTO aProyectLeaderUserDTO)
+			throws ProyectAlreadyExistsException, UnknownUserException {
+
 		try {
 			this.getProyectRespository().getProyectByName(aProyectName);
-			
-		} catch (UnknownProyectException unknownProyectException) {			
+
+		} catch (UnknownProyectException unknownProyectException) {
 			ItemTracker theItemTracker = this.getItemTrackerRespository().getItemTracker();
-			
-		
+
 			User aProyectLeaderUser = this.getUserRespository().getUserByDTO(aProyectLeaderUserDTO);
 			Proyect aProyect = new Proyect(aProyectName, aProyectLeaderUser);
 			theItemTracker.addProyect(aProyect);
-			
+
 			ProyectDTO proyectDTO = (ProyectDTO) ProyectDTOFactory.getInstance().getDTO(aProyect);
 			return proyectDTO;
 		}
-		throw new ProyectAlreadyExistsException("El proyecto "+aProyectName+" ya existe.");
+		throw new ProyectAlreadyExistsException("El proyecto " + aProyectName + " ya existe.");
 	}
-	
+
 	@Override
-	public ProyectDTO getProyect(ProyectDTO aProyectDTO) throws UnknownProyectException{
+	public ProyectDTO getProyect(ProyectDTO aProyectDTO) throws UnknownProyectException {
 		Proyect aProyect = this.getProyectRespository().getProyectByDTO(aProyectDTO);
 		return (ProyectDTO) ProyectDTOFactory.getInstance().getDTO(aProyect);
 	}
-	
+
 	@Override
-	public void addUsersToProyect(ProyectDTO aProyectDTO, Collection<UserDTOForLists> usersDTOs) throws UnknownProyectException, UnknownUserException{
-		
+	public void addUsersToProyect(ProyectDTO aProyectDTO, Collection<UserDTOForLists> usersDTOs)
+			throws UnknownProyectException, UnknownUserException {
+
 		Proyect aProyect = this.getProyectRespository().getProyectByDTO(aProyectDTO);
 		Collection<User> users = this.getUserRespository().getUsersByDTOsList(usersDTOs);
 		aProyect.addUsers(users);
@@ -60,10 +61,11 @@ public class ProyectServiceImpl extends AbstractServiceImpl implements ProyectSe
 
 	@SuppressWarnings("deprecation")
 	@Override
-	public void removeProyect(ProyectDTO anProyectDTOToRemove) throws UnknownProyectException {
-		
+	public void removeProyect(ProyectDTO aProyectDTOToRemove) throws UnknownProyectException, DTOConcurrencyException {
+
 		ItemTracker theItemTracker = this.getItemTrackerRespository().getItemTracker();
-		Proyect aProyect = this.getProyectRespository().getProyectByDTO(anProyectDTOToRemove);
-		theItemTracker.removeProyect(aProyect);
+		Proyect aProyectToRemove = this.getProyectRespository().getProyectByDTO(aProyectDTOToRemove);
+		this.checkDTOConcurrency(aProyectDTOToRemove, aProyectToRemove);
+		theItemTracker.removeProyect(aProyectToRemove);
 	}
 }
