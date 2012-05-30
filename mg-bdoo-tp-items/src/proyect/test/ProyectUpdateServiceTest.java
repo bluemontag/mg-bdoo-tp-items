e7d5951aafefe665fe004689fe54e01755e31f81
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-30 05:51:24 +0000

    nuevo test para probar update de proyectos

--- mg-bdoo-tp-items/src/proyect/test/ProyectUpdateServiceTest.java
+++ mg-bdoo-tp-items/src/proyect/test/ProyectUpdateServiceTest.java
@@ -6,7 +6,12 @@ import java.util.HashSet;
 import org.junit.After;
 import org.junit.Before;
 
+import proyect.exception.UnknownProyectException;
+import base.exception.DTOConcurrencyException;
+
 import user.dto.UserDTO;
+import user.exception.UnknownUserException;
+import user.exception.UserAlreadyExistsException;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
@@ -14,6 +19,9 @@ import user.dto.UserDTO;
 public class ProyectUpdateServiceTest extends ProyectServiceTest {
 
 	Collection<UserDTO> usuariosAAsignarAProyecto = new HashSet<UserDTO>();
+	protected static final int AMOUNT_OF_USERS_TO_SET_IN_PROYECT = 15;
+	protected static final String BASE_USERS_NAME_TO_SET_IN_PROYECT = "user_setted_on_proyect_";
+	protected static final String UPDATED_PROYECT_NAME = "A proyect name UPDATED";
 
 	@Override
 	@Before
@@ -22,6 +30,19 @@ public class ProyectUpdateServiceTest extends ProyectServiceTest {
 		super.setUp();
 		this.createUserProyectLeader();
 		this.createProyect();
+		this.createUsersToSetToAProyect();
+	}
+
+	protected void createUsersToSetToAProyect() {
+		for (int i = 0; i < AMOUNT_OF_USERS_TO_SET_IN_PROYECT; i++) {
+			UserDTO aCreatedUserDTO;
+			try {
+				aCreatedUserDTO = this.userService.createUser(BASE_USERS_NAME_TO_SET_IN_PROYECT + i, "no importa");
+				usuariosAAsignarAProyecto.add(aCreatedUserDTO);
+			} catch (UserAlreadyExistsException e) {
+				fail("El usuario que se intenta crear" + (BASE_USERS_NAME_TO_SET_IN_PROYECT + i) + " ya existe.");
+			}
+		}
 	}
 
 	@Override
@@ -33,6 +54,19 @@ public class ProyectUpdateServiceTest extends ProyectServiceTest {
 	}
 
 	public void testUpdateProyect() {
-
+		this.aCreatedProyectDTO.setUsers(usuariosAAsignarAProyecto);
+		this.aCreatedProyectDTO.setName(UPDATED_PROYECT_NAME);
+		try {
+			this.proyectService.updateProyect(this.aCreatedProyectDTO);
+		} catch (UnknownProyectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DTOConcurrencyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownUserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
