89e77fb936155a0f53ac6d407871dfeca16a73f0
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-30 04:55:34 +0000

    test de update y create proyecto

--- mg-bdoo-tp-items/src/proyect/test/ProyectCreateServiceTest.java
+++ mg-bdoo-tp-items/src/proyect/test/ProyectCreateServiceTest.java
@@ -3,59 +3,44 @@ package proyect.test;
 import org.junit.After;
 import org.junit.Before;
 
-import proyect.dto.ProyectDTO;
 import proyect.exception.ProyectAlreadyExistsException;
-import user.dto.UserDTO;
 import user.exception.UnknownUserException;
-import user.exception.UserAlreadyExistsException;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
  */
 public class ProyectCreateServiceTest extends ProyectServiceTest {
 
-	public final static String NEW_PROYECT_NAME = "A proyect name";
-
 	@Override
 	@Before
 	public void setUp() throws Exception {
 
 		super.setUp();
-		try {
-			// se crea un usuario para setear como lider del proyecto.
-			this.aCreatedProyectLeaderUserDTO = this.userService.createUser("userProyectLeaderTest", "password1");
-		} catch (UserAlreadyExistsException e) {
-			fail("El usuario que se intenta crear ya existe.");
-		}
+		this.createUserProyectLeader();
 	}
 
 	@Override
 	@SuppressWarnings("deprecation")
 	@After
 	public void tearDown() throws Exception {
-
-		ProyectDTO aProyectDTOToRemove = this.proyectService.getProyect(this.aCreatedProyectDTO);
-		UserDTO anUserDTOToRemove = this.userService.getUser(this.aCreatedProyectLeaderUserDTO);
-
-		// Se elimina el proyecto para dejar la base como estaba.
-		this.proyectService.removeProyect(aProyectDTOToRemove);
-
-		// Se eliminan los usuarios creados, hay que dejar la base como estaba.
-		this.userService.removeUser(anUserDTOToRemove);
+		// ojo que no se puede eliminar primero el usuario!
+		this.deleteCreatedProyect();
+		this.deleteCreatedUserProyectLeader();
 	}
 
 	public void testCreateProyect() {
-		ProyectDTO aCreatedProyectDTO = null;
+		this.aCreatedProyectDTO = null;
 		try {
-			aCreatedProyectDTO = this.proyectService.createProyect(NEW_PROYECT_NAME, this.aCreatedProyectLeaderUserDTO);
+			this.aCreatedProyectDTO = this.proyectService.createProyect(NEW_PROYECT_NAME,
+					this.aCreatedProyectLeaderUserDTO);
 		} catch (ProyectAlreadyExistsException e) {
 			fail("El proyecto que se intenta crear ya existe.");
 		} catch (UnknownUserException e) {
 			fail("El usuario que se intenta asignar como lider no existe.");
 		}
-		assertNotNull(aCreatedProyectDTO);
-		assertEquals(NEW_PROYECT_NAME, aCreatedProyectDTO.getName());
-		assertEquals(this.aCreatedProyectLeaderUserDTO.getUserName(), aCreatedProyectDTO.getLeader().getUserName());
+		assertNotNull(this.aCreatedProyectDTO);
+		assertEquals(NEW_PROYECT_NAME, this.aCreatedProyectDTO.getName());
+		assertEquals(this.aCreatedProyectLeaderUserDTO.getUserName(), this.aCreatedProyectDTO.getLeader().getUserName());
 	}
 
 }
--- mg-bdoo-tp-items/src/proyect/test/ProyectServiceTest.java
+++ mg-bdoo-tp-items/src/proyect/test/ProyectServiceTest.java
@@ -1,6 +1,5 @@
 package proyect.test;
 
-
 import junit.framework.TestCase;
 
 import org.junit.After;
@@ -9,23 +8,30 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 
 import proyect.dto.ProyectDTO;
+import proyect.exception.ProyectAlreadyExistsException;
+import proyect.exception.UnknownProyectException;
 import proyect.service.ProyectServiceBI;
 import user.dto.UserDTO;
+import user.exception.UnknownUserException;
+import user.exception.UserAlreadyExistsException;
 import user.service.UserServiceBI;
-
+import base.exception.DTOConcurrencyException;
 import base.service.ServiceFinder;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
  */
-public abstract class ProyectServiceTest extends TestCase{
+public abstract class ProyectServiceTest extends TestCase {
 
 	protected ProyectDTO aCreatedProyectDTO;
 	protected UserDTO aCreatedProyectLeaderUserDTO;
-	
+
 	protected ProyectServiceBI proyectService;
 	protected UserServiceBI userService;
-	
+
+	protected final static String NEW_PROYECT_NAME = "A proyect name";
+	protected final static String NEW_USER_NAME = "userProyectLeaderTest";
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -34,13 +40,49 @@ public abstract class ProyectServiceTest extends TestCase{
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	@Override
 	@Before
 	public void setUp() throws Exception {
 		this.proyectService = ServiceFinder.getInstance().getProyectService();
 		this.userService = ServiceFinder.getInstance().getUserService();
 	}
 
+	@Override
 	@After
 	public void tearDown() throws Exception {
+	}
+
+	protected void createProyect() {
+		try {
+			// se crea el proyecto que se va a updetear
+			this.aCreatedProyectDTO = this.proyectService.createProyect(NEW_PROYECT_NAME,
+					this.aCreatedProyectLeaderUserDTO);
+		} catch (ProyectAlreadyExistsException e) {
+			fail("El proyecto que se intenta crear ya existe.");
+		} catch (UnknownUserException e) {
+			fail("El usuario que se intenta asignar como lider no existe.");
+		}
+	}
+
+	protected void createUserProyectLeader() {
+		try {
+			// se crea un usuario para setear como lider del proyecto.
+			this.aCreatedProyectLeaderUserDTO = this.userService.createUser(NEW_USER_NAME, "password1");
+		} catch (UserAlreadyExistsException e) {
+			fail("El usuario que se intenta crear ya existe.");
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	protected void deleteCreatedUserProyectLeader() throws UnknownUserException, DTOConcurrencyException {
+		// Se eliminan los usuarios creados, hay que dejar la base como estaba.
+		UserDTO anUserDTOToRemove = this.userService.getUser(this.aCreatedProyectLeaderUserDTO);
+		this.userService.removeUser(anUserDTOToRemove);
+	}
+
+	protected void deleteCreatedProyect() throws UnknownProyectException, DTOConcurrencyException {
+		ProyectDTO aProyectDTOToRemove = this.proyectService.getProyect(this.aCreatedProyectDTO);
+		// Se elimina el proyecto para dejar la base como estaba.
+		this.proyectService.removeProyect(aProyectDTOToRemove);
 	}
 }
