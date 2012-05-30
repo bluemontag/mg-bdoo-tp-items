784ef5d145eb6b6e5a356a449ce63af89bf8c598
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-05-30 04:55:20 +0000

    refactor

--- mg-bdoo-tp-items/src/itemTracker/domain/ItemTracker.java
+++ mg-bdoo-tp-items/src/itemTracker/domain/ItemTracker.java
@@ -5,33 +5,31 @@ import java.util.HashSet;
 
 import proyect.domain.Proyect;
 import proyect.exception.UnknownProyectException;
-
-import base.domain.BaseDomain;
-
 import user.domain.User;
 import user.exception.UnknownUserException;
+import base.domain.BaseDomain;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
  */
-public class ItemTracker extends BaseDomain{
-	
+public class ItemTracker extends BaseDomain {
+
 	public final static String Name = "Seguimiento de items";
 	public final static String DESCRIPTION = "Trabajo practico - Mg. Ing. de Softare - BDOO";
-		
+
 	private Collection<User> users = new HashSet<User>();
 	private User adminUser = null;
 	private Collection<Proyect> proyects = new HashSet<Proyect>();
 
-	public ItemTracker(){
+	public ItemTracker() {
 		this.users = new HashSet<User>();
 		this.setProyects(new HashSet<Proyect>());
 	}
-	
+
 	public void addUser(User aUser) {
 		this.users.add(aUser);
 	}
-	
+
 	public void setUsers(Collection<User> users) {
 		this.users = users;
 	}
@@ -47,7 +45,7 @@ public class ItemTracker extends BaseDomain{
 	public void setAdminUser(User theAdminUser) {
 		this.adminUser = theAdminUser;
 	}
-	
+
 	public void setProyects(Collection<Proyect> proyects) {
 		this.proyects = proyects;
 	}
@@ -55,43 +53,33 @@ public class ItemTracker extends BaseDomain{
 	public Collection<Proyect> getProyects() {
 		return proyects;
 	}
-	
-	public void addProyect(Proyect aProyect){
+
+	public void addProyect(Proyect aProyect) {
 		this.proyects.add(aProyect);
 	}
 
 	public void logicalRemoveUser(User anUser) {
 		anUser.setRemoved(true);
 	}
-	
-	//usado solo por los tests para dejar la base como estaba
+
+	// usado solo por los tests para dejar la base como estaba
 	@Deprecated
 	public void removeUser(User anUser) throws UnknownUserException {
-//		User anUserToRemove = null;
-//		for(User user: this.users){
-//			if(user.equals(anUser)){
-//				anUserToRemove = user;
-//				break;
-//			}
-//		}
-//		if(anUserToRemove != null){
 		boolean removed = this.users.remove(anUser);
-//		}else{
-		if(!removed){
+		if (!removed) {
 			throw new UnknownUserException();
 		}
-//		}
 	}
-	
+
 	public void logicalRemoveProyect(Proyect aProyect) {
 		aProyect.setRemoved(true);
 	}
 
-	@Deprecated
+	// Los proyectos tiene eliminacion fisica.
 	public void removeProyect(Proyect aProyect) throws UnknownProyectException {
-		
+
 		boolean removed = this.proyects.remove(aProyect);
-		if(!removed){
+		if (!removed) {
 			throw new UnknownProyectException("El proyecto que desea eliminar no existe.");
 		}
 	}
