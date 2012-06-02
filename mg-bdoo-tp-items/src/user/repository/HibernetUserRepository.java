575ed348b125f48b6dccf9941e3d7e4d60820a5e
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-06-02 07:15:16 +0000

    efactor + correcion del metodo getUserByDTOList

--- mg-bdoo-tp-items/src/user/repository/HibernetUserRepository.java
+++ mg-bdoo-tp-items/src/user/repository/HibernetUserRepository.java
@@ -1,19 +1,18 @@
 package user.repository;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.HashSet;
 
 import org.hibernate.Query;
 
-import base.exception.BaseException;
-import base.repository.HibernateBaseRepository;
 import user.domain.User;
 import user.dto.UserDTO;
 import user.dto.UserDTOForLists;
 import user.exception.UnknownUserException;
+import base.exception.BaseException;
+import base.repository.HibernateBaseRepository;
 
-public class HibernetUserRepository extends HibernateBaseRepository implements UserRepositoryBI{
-
+public class HibernetUserRepository extends HibernateBaseRepository implements UserRepositoryBI {
 
 	@SuppressWarnings("rawtypes")
 	@Override
@@ -24,12 +23,12 @@ public class HibernetUserRepository extends HibernateBaseRepository implements U
 	@Override
 	public User getUserByOid(String anOid) throws UnknownUserException {
 		User anUser = (User) this.findeByOid(this.getEntityClass(), anOid);
-		if(anUser == null){
+		if (anUser == null) {
 			throw new UnknownUserException();
 		}
 		return anUser;
-	} 
-	
+	}
+
 	@Override
 	public User getUserByUserName(String anUserName) throws UnknownUserException {
 
@@ -40,7 +39,7 @@ public class HibernetUserRepository extends HibernateBaseRepository implements U
 			// TODO ver que hacer!!
 		}
 		if (user == null) {
-				throw new UnknownUserException("El usuario "+ anUserName +" no existe.");
+			throw new UnknownUserException("El usuario " + anUserName + " no existe.");
 		}
 		return user;
 	}
@@ -55,21 +54,21 @@ public class HibernetUserRepository extends HibernateBaseRepository implements U
 	public Collection<User> getUsersByDTOsList(Collection<UserDTOForLists> usersDTOs) throws UnknownUserException {
 
 		Collection<String> oids = this.getOidsFromColleccionOfDTOs(usersDTOs);
-		
+
 		Query aQuery = this.getNamedQuery("getUsersByOids");
 
 		aQuery.setParameterList("oids", oids);
 		aQuery.setParameter("isRemoved", false);
 
-		Collection<User> users = (Collection<User>) aQuery.list();
+		Collection<User> users = aQuery.list();
 
 		if (users == null) {
 			throw new UnknownUserException("No se encontro ningun usuario de la lista.");
 		}
-		if(users.size() != usersDTOs.size()){
+		if (users.size() != usersDTOs.size()) {
 			throw new UnknownUserException("Alguno de los usuarios de la lista no se encontro o fue eliminado.");
 		}
-		return users;
-		
+		return new HashSet<User>(users);
+
 	}
 }
