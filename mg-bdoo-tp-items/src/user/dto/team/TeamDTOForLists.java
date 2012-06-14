453021979add89fdce59bd4bf1bc2b45ec12e763
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-06-14 02:24:14 +0000

    dtos para team

---
+++ mg-bdoo-tp-items/src/user/dto/team/TeamDTOForLists.java
@@ -0,0 +1,32 @@
+package user.dto.team;
+
+import user.domain.team.Team;
+import base.dto.AbstractDTOForLists;
+
+public class TeamDTOForLists extends AbstractDTOForLists {
+
+	private String name;
+
+	public TeamDTOForLists(Team aTeam) {
+		super(aTeam);
+		this.name = aTeam.getName();
+	}
+
+	public TeamDTOForLists(TeamDTO aTeamDTO) {
+		super(aTeamDTO);
+		this.name = aTeamDTO.getName();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String aName) {
+		this.name = aName;
+	}
+
+	@Override
+	public String toString() {
+		return "Id: " + this.getOid() + " - Team Name: " + this.getName();
+	}
+}
