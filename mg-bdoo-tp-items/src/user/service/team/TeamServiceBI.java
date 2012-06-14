ecb310d02609f28ce0496dd64082ecff8026adb4
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-06-14 02:25:12 +0000

    servicios de team

--- mg-bdoo-tp-items/src/user/service/team/TeamServiceBI.java
+++ mg-bdoo-tp-items/src/user/service/team/TeamServiceBI.java
@@ -1,5 +1,13 @@
 package user.service.team;
 
+import java.util.Collection;
+
+import user.dto.UserDTOForLists;
+import user.dto.team.TeamDTO;
+import user.exception.UnknownUserException;
+import user.exception.team.TeamAlreadyExistsException;
+import user.exception.team.UnknownTeamException;
+import base.exception.DTOConcurrencyException;
 
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
@@ -8,6 +16,9 @@ public interface TeamServiceBI {
 
 	// Creats
 
+	TeamDTO createTeam(String sessionToken, String aTeamName, Collection<UserDTOForLists> usersDTOs)
+			throws UnknownUserException, TeamAlreadyExistsException;
+
 	// Lists
 
 	// Retrives
@@ -15,6 +26,8 @@ public interface TeamServiceBI {
 	// Updates
 
 	// Removes
+
+	void removeTeam(String sessionToken, TeamDTO aTeamDTOToRemove) throws DTOConcurrencyException, UnknownTeamException;
 
 	// usado por los tests para dejar la base como estaba
 	// @Deprecated
--- mg-bdoo-tp-items/src/user/service/team/TeamServiceImpl.java
+++ mg-bdoo-tp-items/src/user/service/team/TeamServiceImpl.java
@@ -1,5 +1,17 @@
 package user.service.team;
 
+import itemTracker.domain.ItemTracker;
+
+import java.util.Collection;
+
+import user.domain.User;
+import user.domain.team.Team;
+import user.dto.UserDTOForLists;
+import user.dto.team.TeamDTO;
+import user.dto.team.TeamDTOFactory;
+import user.exception.UnknownUserException;
+import user.exception.team.UnknownTeamException;
+import base.exception.DTOConcurrencyException;
 import base.service.AbstractServiceImpl;
 
 /**
@@ -7,4 +19,24 @@ import base.service.AbstractServiceImpl;
  */
 public class TeamServiceImpl extends AbstractServiceImpl implements TeamServiceBI {
 
+	@Override
+	public TeamDTO createTeam(String sessionToken, String aTeamName, Collection<UserDTOForLists> usersDTOs)
+			throws UnknownUserException {
+
+		ItemTracker theItemTracker = this.getItemTrackerRespository().getItemTracker();
+		Collection<User> users = this.getUserRespository().getUsersByDTOsList(usersDTOs);
+		Team newTeam = new Team(aTeamName, users);
+		theItemTracker.addTeam(newTeam);
+		return (TeamDTO) TeamDTOFactory.getInstance().getDTO(newTeam);
+	}
+
+	@Override
+	public void removeTeam(String sessionToken, TeamDTO aTeamDTOToRemove) throws DTOConcurrencyException,
+			UnknownTeamException {
+		ItemTracker theItemTracker = this.getItemTrackerRespository().getItemTracker();
+		Team aTeam = this.getTeamRespository().getTeamByDTO(aTeamDTOToRemove);
+		this.checkDTOConcurrency(aTeamDTOToRemove, aTeam);
+		theItemTracker.removeTeam(aTeam);
+
+	}
 }
