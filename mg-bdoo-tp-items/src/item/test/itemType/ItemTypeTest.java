62169fd3e40b5924e79739827973f26ff1e56716
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-10-03 03:07:25 +0000

    el contexto se carga en BaseTest

--- mg-bdoo-tp-items/src/item/test/itemType/ItemTypeTest.java
+++ mg-bdoo-tp-items/src/item/test/itemType/ItemTypeTest.java
@@ -3,7 +3,6 @@
  */
 package item.test.itemType;
 
-
 import item.dto.itemType.ItemTypeDTO;
 import item.exception.itemType.ItemTypeAlreadyExistsException;
 import junit.framework.Test;
@@ -21,8 +20,8 @@ import base.test.BaseTestCase;
 
 /**
  * @author igallego ignaciogallego@gmail.com
- *  
- * 03/07/2012
+ * 
+ *         03/07/2012
  */
 public class ItemTypeTest extends BaseTestCase {
 
@@ -30,38 +29,36 @@ public class ItemTypeTest extends BaseTestCase {
 	private TeamDTO tDTO;
 	private WorkflowDTO wDTO;
 	private ItemTypeDTO itemTypeDTO;
-	
-	public static Test suite(){
+
+	public static Test suite() {
 		/* Crea el test para ejecutarlo desde el IDE */
 		return new TestSuite(ItemTypeTest.class);
 	}
-	
+
 	@Override
 	public void setUp() throws Exception {
-		/* Setea todo para correr el test */	
-		this.setServices();
-		super.setUp(); //crea y loguea  usuario
+		super.setUp(); // crea y loguea usuario
 		this.createAUserCollection();
 	}
 
-	public void testItemTypeCreation() throws UnknownUserException, TeamAlreadyExistsException, WorkflowAlreadyExistsException {
-		
+	public void testItemTypeCreation() throws UnknownUserException, TeamAlreadyExistsException,
+			WorkflowAlreadyExistsException {
+
 		tDTO = this.teamService.createTeam(this.sessionToken, "Equipo Ignacio", this.aUserDTOForListCollection);
-		
+
 		wDTO = this.workflowService.createWorkflow(this.sessionToken, "WF Ignacio");
 
-		
-		//agrego estados
+		// agrego estados
 		pendiente = this.getItemState("Pendiente");
 		ItemStateDTO desa = this.getItemState("En desarrollo");
 		ItemStateDTO finalizado = this.getItemState("Finalizado");
 
-		//Intento setear los proximos estados
+		// Intento setear los proximos estados
 		this.addNextState(pendiente, desa);
 		this.addNextState(desa, finalizado);
 		this.addNextState(pendiente, finalizado);
-		
-		//Seteo el estado inicial al WF
+
+		// Seteo el estado inicial al WF
 		try {
 			this.getWorkflowService().setInitialState(this.sessionToken, wDTO, pendiente);
 		} catch (UnknownWorkflowException e) {
@@ -70,31 +67,36 @@ public class ItemTypeTest extends BaseTestCase {
 			System.out.println("El estado inicial es desconocido.");
 		}
 
-		
-		
-		//intento crear el IT
+		// intento crear el IT
 		try {
 			itemTypeDTO = this.getItemTypeService().createItemType(this.sessionToken, "Tipo Basico", tDTO, wDTO);
-		} catch (UnknownWorkflowException e) { e.printStackTrace(); fail("UnknownWorkflow");}
-		 catch (ItemTypeAlreadyExistsException e) {e.printStackTrace();fail("Item existe");}
-		catch (UnknownTeamException e) {e.printStackTrace(); fail("Team desconocido");}
+		} catch (UnknownWorkflowException e) {
+			e.printStackTrace();
+			fail("UnknownWorkflow");
+		} catch (ItemTypeAlreadyExistsException e) {
+			e.printStackTrace();
+			fail("Item existe");
+		} catch (UnknownTeamException e) {
+			e.printStackTrace();
+			fail("Team desconocido");
+		}
 	}
-	
-	
+
 	@Override
 	protected void tearDown() throws Exception {
-		//leo el DTO antes de borrar para evitar el DTOConcurrencyException
+		// leo el DTO antes de borrar para evitar el DTOConcurrencyException
 		itemTypeDTO = this.getItemTypeService().getItemType(this.sessionToken, itemTypeDTO);
 		this.itemTypeService.removeItemType(this.sessionToken, itemTypeDTO);
-		
+
 		this.wDTO = this.getWorkflowService().getWorkflowByDTO(this.sessionToken, wDTO);
 		this.getWorkflowService().removeWorkflow(this.sessionToken, this.wDTO);
-	
+
 		this.tDTO = this.getTeamService().getTeam(this.sessionToken, tDTO);
 		this.teamService.removeTeam(this.sessionToken, tDTO);
-		
-		//no es necesario la siguiente linea, porque borro en cascada en la coleccion de usuarios
-		//this.deleteTheUserCollection();
+
+		// no es necesario la siguiente linea, porque borro en cascada en la
+		// coleccion de usuarios
+		// this.deleteTheUserCollection();
 		super.tearDown();
 	}
 }
