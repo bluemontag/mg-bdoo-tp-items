e7a01aa87f14c0a72d0cc0eac17526323e6ffd2a
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-04 22:12:53 +0000

    se cambia el momento en que se settea el OID. Cada obejeto que hereda de BaseDomain debe llamar a super() en el constructor. + Refactor

--- mg-bdoo-tp-items/src/item/domain/HistoricItem.java
+++ mg-bdoo-tp-items/src/item/domain/HistoricItem.java
@@ -4,11 +4,12 @@
 package item.domain;
 
 import java.util.Date;
+
 import base.domain.BaseDomain;
 
 /**
  * @author igallego ignaciogallego@gmail.com
- *
+ * 
  */
 public class HistoricItem extends BaseDomain {
 
@@ -18,64 +19,54 @@ public class HistoricItem extends BaseDomain {
 	private String responsibleUserName;
 	private Date date;
 
-	
-	/**
-	 * @return the type
-	 */
+	public HistoricItem() {
+	}
+
+	public HistoricItem(Item item) {
+		super();
+		this.typeName = item.getType().getTypeName();
+		this.itemNum = item.getItemNum();
+		this.stateName = item.getCurrentState().getName();
+		this.responsibleUserName = item.getResponsible().getUserName();
+		this.date = new Date();
+	}
+
 	public String getTypeName() {
 		return typeName;
 	}
-	/**
-	 * @param type the type to set
-	 */
+
 	public void setTypeName(String type) {
 		this.typeName = type;
 	}
-	/**
-	 * @return the itemNum
-	 */
+
 	public Long getItemNum() {
 		return itemNum;
 	}
-	/**
-	 * @param itemNum the itemNum to set
-	 */
+
 	public void setItemNum(Long itemNum) {
 		this.itemNum = itemNum;
 	}
-	/**
-	 * @return the stateName
-	 */
+
 	public String getStateName() {
 		return stateName;
 	}
-	/**
-	 * @param stateName the stateName to set
-	 */
+
 	public void setStateName(String stateName) {
 		this.stateName = stateName;
 	}
-	/**
-	 * @return the responsibleUserName
-	 */
+
 	public String getResponsibleUserName() {
 		return responsibleUserName;
 	}
-	/**
-	 * @param responsibleUserName the responsibleUserName to set
-	 */
+
 	public void setResponsibleUserName(String responsibleUserName) {
 		this.responsibleUserName = responsibleUserName;
 	}
-	/**
-	 * @return the date
-	 */
+
 	public Date getDate() {
 		return date;
 	}
-	/**
-	 * @param date the date to set
-	 */
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
