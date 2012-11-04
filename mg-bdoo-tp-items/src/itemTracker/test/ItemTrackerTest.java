a5c5e263bb288f1423ddc85981107145408bfa05
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-04 22:14:29 +0000

    El metodo estatico getClassesTestToPerform debe devolver todos los test relacionados con el dominio al que esta asociado.

--- mg-bdoo-tp-items/src/itemTracker/test/ItemTrackerTest.java
+++ mg-bdoo-tp-items/src/itemTracker/test/ItemTrackerTest.java
@@ -1,5 +1,7 @@
 package itemTracker.test;
 
+import java.util.ArrayList;
+import java.util.Collection;
 
 import junit.framework.TestCase;
 
@@ -11,7 +13,7 @@ import org.junit.BeforeClass;
 /**
  * @author Rodrigo Itursarry (itursarry@gmail.com)
  */
-public class ItemTrackerTest extends TestCase{
+public class ItemTrackerTest extends TestCase {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -21,12 +23,20 @@ public class ItemTrackerTest extends TestCase{
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	@Override
 	@Before
 	public void setUp() throws Exception {
 	}
 
+	@Override
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	@SuppressWarnings("rawtypes")
+	public static Collection<Class> getClassesTestToPerform() {
+		// en este metodo se agregan todos los test a realizar relacionados
+		Collection<Class> itemTrackerTestsClasses = new ArrayList<Class>();
+		return itemTrackerTestsClasses;
+	}
 }
