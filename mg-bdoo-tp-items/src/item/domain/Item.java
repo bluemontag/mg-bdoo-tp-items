20002a4592e66cd1fc5b9fec20c774b5ba1d39e5
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-04 22:13:26 +0000

    se cambia el momento en que se settea el OID. Cada obejeto que hereda de BaseDomain debe llamar a super() en el constructor. + Eliminacion de comentarios no explicativos

--- mg-bdoo-tp-items/src/item/domain/Item.java
+++ mg-bdoo-tp-items/src/item/domain/Item.java
@@ -7,7 +7,6 @@ import item.domain.itemType.ItemType;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 
 import user.domain.User;
@@ -17,7 +16,7 @@ import base.domain.BaseDomain;
 
 /**
  * @author igallego ignaciogallego@gmail.com
- *
+ * 
  */
 public class Item extends BaseDomain {
 
@@ -26,54 +25,51 @@ public class Item extends BaseDomain {
 	private Integer priority;
 	private ItemState currentState;
 	private ItemType type;
-	private User responsible; //(se escribe asi "responsible")
+	private User responsible; // (se escribe asi "responsible")
 	private Collection<HistoricItem> history = new ArrayList<HistoricItem>();
-	
 
 	@Deprecated
 	public Item(Long itemNum, String typeName) {
 		/* Creation of an item without workflow... is useless! */
 		this(itemNum, "", 1, new ItemType(typeName, null, null));
 	}
-	
+
 	public Item() {
-		//para hibernate
+		// para hibernate
 	}
+
 	public Item(Long itemNum, String description, Integer priority, ItemType type) {
-		this.itemNum = itemNum; 
+		super();
+		this.itemNum = itemNum;
 		this.description = description;
 		this.priority = priority;
 		this.type = type;
 		this.responsible = null;
-		this.currentState = null; //new ItemState("new");
+		this.currentState = null; // new ItemState("new");
 	}
+
 	/**
 	 * Takes a snapshot of the current state of the Item, and saves it for
-	 *  further reference.
+	 * further reference.
 	 * 
-	 * Toma una instantanea del estado actual del Item, y la guarda
-	 *  para futura referencia.
-	 *   
+	 * Toma una instantanea del estado actual del Item, y la guarda para futura
+	 * referencia.
+	 * 
 	 * @throws Exception
 	 */
 	public HistoricItem saveItem() throws Exception {
-		HistoricItem hi = new HistoricItem();
-		hi.setDate(new Date());
-		hi.setItemNum(this.getItemNum());
-		hi.setResponsibleUserName(this.getResponsible().getUserName());
-		hi.setStateName(this.getCurrentState().getName());
-		hi.setTypeName(this.getType().getTypeName());
-		
-		//save in history
+		HistoricItem hi = new HistoricItem(this);
+
+		// save in history
 		this.history.add(hi);
-		
+
 		return hi;
-		
+
 	}
-	
+
 	/**
-	 * Executes the transition and saves the new current state, 
-	 *  obtained from the transition.
+	 * Executes the transition and saves the new current state, obtained from
+	 * the transition.
 	 * 
 	 * @param t
 	 * @throws BadTransitionException
@@ -83,6 +79,7 @@ public class Item extends BaseDomain {
 		this.setCurrentState(newState);
 		return newState;
 	}
+
 	/**
 	 * @return the type
 	 */
@@ -91,7 +88,8 @@ public class Item extends BaseDomain {
 	}
 
 	/**
-	 * @param type the type to set
+	 * @param type
+	 *            the type to set
 	 */
 	public void setType(ItemType type) {
 		this.type = type;
@@ -105,7 +103,8 @@ public class Item extends BaseDomain {
 	}
 
 	/**
-	 * @param itemNum the itemNum to set
+	 * @param itemNum
+	 *            the itemNum to set
 	 */
 	public void setItemNum(Long itemNum) {
 		this.itemNum = itemNum;
@@ -119,7 +118,8 @@ public class Item extends BaseDomain {
 	}
 
 	/**
-	 * @param description the description to set
+	 * @param description
+	 *            the description to set
 	 */
 	public void setDescription(String description) {
 		this.description = description;
@@ -133,7 +133,8 @@ public class Item extends BaseDomain {
 	}
 
 	/**
-	 * @param priority the priority to set
+	 * @param priority
+	 *            the priority to set
 	 */
 	public void setPriority(Integer priority) {
 		this.priority = priority;
@@ -147,7 +148,8 @@ public class Item extends BaseDomain {
 	}
 
 	/**
-	 * @param state the state to set
+	 * @param state
+	 *            the state to set
 	 */
 	public void setCurrentState(ItemState state) {
 		this.currentState = state;
@@ -161,7 +163,8 @@ public class Item extends BaseDomain {
 	}
 
 	/**
-	 * @param responsible the responsible to set
+	 * @param responsible
+	 *            the responsible to set
 	 */
 	public void setResponsible(User responsible) {
 		this.responsible = responsible;
@@ -175,7 +178,8 @@ public class Item extends BaseDomain {
 	}
 
 	/**
-	 * @param history the history to set
+	 * @param history
+	 *            the history to set
 	 */
 	public void setHistory(List<HistoricItem> history) {
 		this.history = history;
