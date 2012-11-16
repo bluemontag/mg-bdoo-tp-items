404cfbb9ded0899baf2d36e03b29841fb8e9959e
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-16 04:01:22 +0000

    se agregan datos necesarios en los DTOs de workflow

--- mg-bdoo-tp-items/src/workflow/dto/WorkflowDTO.java
+++ mg-bdoo-tp-items/src/workflow/dto/WorkflowDTO.java
@@ -1,6 +1,11 @@
 package workflow.dto;
 
+import java.util.Collection;
+
 import workflow.domain.Workflow;
+import workflow.domain.state.ItemState;
+import workflow.dto.state.ItemStateDTOFactory;
+import workflow.dto.state.ItemStateDTOForLists;
 import base.dto.AbstractDTO;
 
 /**
@@ -10,11 +15,17 @@ public class WorkflowDTO extends AbstractDTO {
 
 	private String name;
 	private boolean removed;
-	
+	private final Collection<ItemStateDTOForLists> itemStates;
+	private final ItemState initialState;
+
+	@SuppressWarnings("unchecked")
 	public WorkflowDTO(Workflow aWorkflow) {
 		super(aWorkflow);
 		this.name = aWorkflow.getName();
 		this.removed = aWorkflow.isRemoved();
+		this.initialState = aWorkflow.getInitialState();
+		this.itemStates = (Collection<ItemStateDTOForLists>) ItemStateDTOFactory.getInstance()
+				.getDTOsForListFromBaseDomains(aWorkflow.getItemStates());
 	}
 
 	public String getName() {
@@ -31,6 +42,14 @@ public class WorkflowDTO extends AbstractDTO {
 
 	public void setRemoved(boolean removed) {
 		this.removed = removed;
+	}
+
+	public Collection<ItemStateDTOForLists> getItemStates() {
+		return itemStates;
+	}
+
+	public ItemState getInitialState() {
+		return initialState;
 	}
 
 }
