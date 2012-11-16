577527475e293ad8ce3d49a2620ed5d977bbc13f
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-16 03:59:03 +0000

    se agregan las transiciones: ahora las transiciones estan contenidas dentro de los estados.

--- mg-bdoo-tp-items/src/workflow/domain/state/ItemState.java
+++ mg-bdoo-tp-items/src/workflow/domain/state/ItemState.java
@@ -1,109 +1,80 @@
-/**
- * 
- */
 package workflow.domain.state;
 
-import java.util.HashMap;
-import java.util.Map;
+import item.domain.Item;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import workflow.domain.transition.Transition;
 import workflow.exception.transition.BadTransitionException;
 import base.domain.BaseDomain;
 
 /**
  * @author igallego
- *
+ * 
  */
 public class ItemState extends BaseDomain {
 
 	private String name;
-	private Map<String, ItemState> childStates;
-	private Map<String, ItemState> parentStates;
+	private Collection<Transition> transitions;
 
 	public ItemState() {
-		this("");
+
 	}
-	
+
 	public ItemState(String name) {
 		super();
 		this.name = name;
-		this.childStates = new HashMap<String, ItemState>();
-		this.parentStates = new HashMap<String, ItemState>();
+		this.transitions = new ArrayList<Transition>();
 	}
-	
-	/**
-	 * Comprueba que la transicion se aplique al estado actual
-	 * y devuelve el estado resultante de aplicar la transicion.
-	 * En caso de que no se aplique , se levanta una excepcion.
-	 * 
-	 * El string "t" debe ser por ejemplo "aDesarrollo", si el existe un estado final llamado "Desarrollo"
-	 * 
-	 * @param finalState
-	 * @throws Exception
-	 */
-	public ItemState executeTransition(String finalState) throws BadTransitionException {
-		if (finalState!=null && finalState.toUpperCase().equals(this.name.toUpperCase())) {
-			//si estoy ya en ese estado, no hago nada
-			return this;//no se aplica la transicion, es decir, queda en el mismo estado
-		} else {
-			//si el estado final existe en la lista de estados finales del estado "this":
-			if (finalState!=null &&	this.childStates.containsKey(finalState.toUpperCase())) {
-				ItemState next = this.getChildStates().get(finalState.toUpperCase());
-				return next;//retorno el estado final
-			} else {
-				throw new BadTransitionException("Final state not found in state:"+this.getName());
-			}
-		}
+
+	public void setTransitions(Collection<Transition> transitions) {
+		this.transitions = transitions;
 	}
-	
+
+	public Collection<Transition> getTransitions() {
+		return transitions;
+	}
+
+	public void addTransition(Transition transition) {
+		this.transitions.add(transition);
+	}
+
 	public Boolean deleted() {
 		return false;
 	}
-	
+
 	public Boolean finalized() {
 		return false;
 	}
 
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	/**
-	 * Adds some state to the transitions table. 
-	 * The table is indexed by states names.
-	 * 
+	 * Cambia de estado un Item y devuelve su estado actual.
 	 */
-	public void addNextState(ItemState s) {
-		this.childStates.put(s.getName().toUpperCase(), s);
-		s.addParentState(this);
+	public void executeTransition(String aTransitionCode, Item anItem) throws BadTransitionException {
+
+		if (aTransitionCode != null) {
+			for (Transition transition : this.transitions) {
+				if (transition.isThisTransition(aTransitionCode)) {
+					this.changeState(transition, anItem);
+					break;
+				}
+			}
+		} else {
+			throw new BadTransitionException("El codigo de traniscion: " + aTransitionCode
+					+ " no se encontro en el estado" + this.name);
+		}
 	}
 
-	public void addParentState(ItemState s) {
-		this.parentStates.put(s.getName().toUpperCase(), s);
-	}
-	
-	public Map<String, ItemState> getChildStates() {
-		return childStates;
-	}
-
-	public void setChildStates(Map<String, ItemState> childStates) {
-		this.childStates = childStates;
-	}
-	
-	public Map<String, ItemState> getParentStates() {
-		return parentStates;
-	}
-
-	public void setParentStates(Map<String, ItemState> parentStates) {
-		this.parentStates = parentStates;
+	public void changeState(Transition transition, Item anItem) {
+		anItem.setCurrentState(transition.getNextState());
 	}
 }
