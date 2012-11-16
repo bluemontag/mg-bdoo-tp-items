025e29c47f74835bf8740b0ca4589759e2d2e5bf
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-16 03:58:00 +0000

    se agregan los itemStates y el initialState ya que ahora los estados estan contenidos en el workflow. Tambien se eliminan comentarios.

--- mg-bdoo-tp-items/src/workflow/domain/Workflow.java
+++ mg-bdoo-tp-items/src/workflow/domain/Workflow.java
@@ -3,7 +3,11 @@
  */
 package workflow.domain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import workflow.domain.state.ItemState;
+import workflow.exception.state.UnknownItemStateException;
 import base.domain.BaseDomain;
 
 /**
@@ -13,44 +17,55 @@ import base.domain.BaseDomain;
  */
 public class Workflow extends BaseDomain {
 
-	private String name;
-	private ItemState initialState;
+	protected String name;
+	protected ItemState initialState;
+	protected Collection<ItemState> itemStates;
 
 	public Workflow() {
+		this.itemStates = new ArrayList<ItemState>();
 	}
 
 	public Workflow(String name) {
 		super();
 		this.name = name;
+		this.itemStates = new ArrayList<ItemState>();
 	}
 
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the initialState
-	 */
 	public ItemState getInitialState() {
 		return initialState;
 	}
 
-	/**
-	 * @param initialState
-	 *            the initialState to set
-	 */
 	public void setInitialState(ItemState initialState) {
 		this.initialState = initialState;
+	}
+
+	public Collection<ItemState> getItemStates() {
+		return itemStates;
+	}
+
+	public void setItemStates(Collection<ItemState> states) {
+		this.itemStates = states;
+	}
+
+	public void addItemState(ItemState anItemState) {
+		this.itemStates.add(anItemState);
+	}
+
+	public void removeItemState(ItemState anItemState) throws UnknownItemStateException {
+		boolean removed = this.itemStates.remove(anItemState);
+		if (!removed) {
+			throw new UnknownItemStateException("El estado del item que desea eliminar no existe.");
+		}
+		if (anItemState.equals(this.initialState)) {
+			this.initialState = null;
+		}
 	}
 }
