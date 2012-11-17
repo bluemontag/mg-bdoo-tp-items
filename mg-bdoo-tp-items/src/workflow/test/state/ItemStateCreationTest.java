9099b3731fbd61e1d9ca40f96e274289a033bc71
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-17 20:07:22 +0000

    Refactor del test de itemState

--- mg-bdoo-tp-items/src/workflow/test/state/ItemStateCreationTest.java
+++ mg-bdoo-tp-items/src/workflow/test/state/ItemStateCreationTest.java
@@ -3,11 +3,6 @@
  */
 package workflow.test.state;
 
-import workflow.dto.state.ItemStateDTO;
-import workflow.exception.UnknownWorkflowException;
-import workflow.exception.state.ItemStateAlreadyExistsException;
-import workflow.exception.state.UnknownItemStateException;
-import base.exception.DTOConcurrencyException;
 import base.test.TestConstants;
 
 /**
@@ -16,9 +11,6 @@ import base.test.TestConstants;
  *         03/07/2012
  */
 public class ItemStateCreationTest extends ItemStateServiceTest {
-
-	protected ItemStateDTO initialStateDTO;
-	protected ItemStateDTO anItemPendingStateDTO;
 
 	@Override
 	public void setUp() throws Exception {
@@ -29,39 +21,17 @@ public class ItemStateCreationTest extends ItemStateServiceTest {
 
 	public void testItemStateCreation() {
 
-		try {
-			this.anItemPendingStateDTO = this.itemStateService.createItemStateOnWorkflow(sessionToken,
-					this.aWorkflowDTO, TestConstants.PENDING, true);
-		} catch (ItemStateAlreadyExistsException e) {
-			fail("El estado de nombre " + TestConstants.PENDING + " ya existe.");
-		} catch (UnknownWorkflowException e) {
-			fail("No existe el workflow de nombre " + this.aWorkflowDTO.getName());
-		} catch (DTOConcurrencyException e) {
-			fail("DTOConcurrencyException: no deberia tirar esta excepcion en un test controlado.");
-		}
-		// se recupera nuevamente el workflow para refrescar.
-		try {
-			this.aWorkflowDTO = this.workflowService.getWorkflowByDTO(sessionToken, this.aWorkflowDTO);
-		} catch (UnknownWorkflowException e) {
-			fail("El workflow " + this.aWorkflowDTO.getName() + " no existe.");
-		}
-
-		assertEquals(this.aWorkflowDTO.getInitialState().getOid(), this.anItemPendingStateDTO.getOid());
-		assertEquals(TestConstants.PENDING, this.anItemPendingStateDTO.getName());
+		this.createItemStateOnWorkflow(TestConstants.PENDING, true);
+		this.refreshWorkflow();
+		assertEquals(this.aWorkflowDTO.getInitialState().getOid(), this.anItemStateDTO.getOid());
+		assertEquals(TestConstants.PENDING, this.anItemStateDTO.getName());
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		try {
-			this.itemStateService.removeItemStateFromWorkflow(sessionToken, this.aWorkflowDTO,
-					this.anItemPendingStateDTO);
-		} catch (UnknownItemStateException e) {
-			fail("No se puede eliminar el estado" + this.anItemPendingStateDTO.getName() + " ya que no existe.");
-		} catch (DTOConcurrencyException e) {
-			fail("DTOConcurrencyException: no deberia tirar esta excepcion en un test controlado.");
-		} catch (UnknownWorkflowException e) {
-			fail("No existe el workflow de nombre " + this.aWorkflowDTO.getName());
-		}
+		this.removeItemStateFromWorkflow(this.anItemStateDTO);
+		this.refreshWorkflow();
+		this.removeWorkflow();
 		super.tearDown();
 	}
 }
