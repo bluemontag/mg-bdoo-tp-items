00af0fa11d3f1bba673d052879a2e38b38982ed2
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-21 02:11:21 +0000

    se lanza esta excepcion cuando se caputa un error de concurrencia de hibernate

--- mg-bdoo-tp-items/src/base/exception/StoreConcurrencyException.java
+++ mg-bdoo-tp-items/src/base/exception/StoreConcurrencyException.java
@@ -1,6 +1,6 @@
 package base.exception;
 
-public class StoreConcurrencyException extends BaseException {
+public class StoreConcurrencyException extends RuntimeException {
 
 	private static final long serialVersionUID = 5563455232880877756L;
 
