4219c90b09a0c520a4313b23e669c81962e66ebd
itursarry <itursarry@33e8253c-54cf-7271-52bc-461b104de33c>
2012-11-21 02:12:36 +0000

    se lanza esta excepcion cuando se caputa un error de concurrencia de hibernate

--- mg-bdoo-tp-items/src/base/security/ServiceSecurityPreInterceptor.java
+++ mg-bdoo-tp-items/src/base/security/ServiceSecurityPreInterceptor.java
@@ -2,7 +2,9 @@ package base.security;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.orm.hibernate4.HibernateOptimisticLockingFailureException;
 
+import base.exception.StoreConcurrencyException;
 import base.exception.UserNotLoggedException;
 
 /**
@@ -16,21 +18,19 @@ public class ServiceSecurityPreInterceptor implements MethodInterceptor {
 		Object[] arguments = invocation.getArguments();
 		String sessionToken = (String) arguments[0];
 		if (itemTrackerSesion.isValidToken(sessionToken)) {
-			// try {
-			Object ret = invocation.proceed();
-			return ret;
-			// } catch (Throwable anyException) {
-			//
-			// if
-			// (anyException.getClass().equals(HibernateOptimisticLockingFailureException.class))
-			// {
-			// throw new
-			// StoreConcurrencyException("La entidad que desea guardar fue modificada por otro usuario.");
-			// } else {
-			// throw anyException;
-			//
-			// }
-			// }
+			try {
+				Object ret = invocation.proceed();
+				return ret;
+			} catch (Throwable anyException) {
+
+				if (anyException.getClass().equals(HibernateOptimisticLockingFailureException.class)) {
+					throw new StoreConcurrencyException(
+							"La entidad que desea actualizar, fue modificar por otro usuario.");
+				} else {
+					throw anyException;
+
+				}
+			}
 		} else {
 			throw new UserNotLoggedException("El usuario no tiene acceso al sistema.");
 		}
